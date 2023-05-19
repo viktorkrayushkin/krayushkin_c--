e076cf2d81e61bbb33dd0aaa10a04d8ea9620d4a
Viktor Krayushkin <100699441+viktorkrayushkin@users.noreply.github.com>
2023-05-19 20:27:41 +0300

    add file

--- krayushkin_c--_corrected/ht_4/matrix/main.cpp
+++ krayushkin_c--_corrected/ht_4/matrix/main.cpp
@@ -1,176 +1,133 @@
 #include <iostream>
-#include <vector>
+#include <array>
+#include <initializer_list>
+
 
 using namespace std;
 
-class Matrix
-{
+template <typename T, int M, int N>
+class Matrix {
 private:
-    vector<vector<int> > matrix;
-    int row;
-    int column;
+    int Rows_n;
+    int Col_n;
+    array<array<T, N>, M> values;
+
 public:
-    Matrix() : row(0), column(0) {}
-    Matrix(int row, int column) : row(row), column(column)
-    {
-        matrix.resize(row, vector<int>(column));
+    int rows_n() const {
+        return Rows_n;
     }
-    void EnterMatrix() //ввод матрицы
-    {
-        int element;
-        for (int i = 0; i < row; i++)
-            for (int j = 0; j < column; j++)
-            {
-                cin >> element;
-                matrix[i][j] = element;
-            }
+
+    int col_n() const {
+        return Col_n;
     }
-    friend Matrix operator+(const Matrix& a, const Matrix& b);
-    friend ostream& operator<<(ostream& out, const Matrix& m);
-    friend Matrix operator*(const Matrix& a, const Matrix& b);
+
+    Matrix() {
+        values = {};
+    }
+    Matrix<T, M, N>& operator=(const Matrix<T, M, N>&);
+    Matrix<T, M, N>& operator=(Matrix<T, M, N>&&);
+    Matrix(initializer_list<initializer_list<T>>);
+    array<T, N>& operator[](int);
+    const array<T, N>& operator[](int) const;
 };
 
-Matrix operator*(const Matrix& a, const Matrix& b)
-{
+template <typename T, int M, int N>
+Matrix<T, M, N>::Matrix(initializer_list<initializer_list<T>> lst) {
+    if (lst.size() != M) {
+        delete this;
+        throw(1);
+    }
 
-    cout << "Произведение матриц равно:" << endl;
-    Matrix result(a.row, b.column);
-    for (int i = 0; i < a.row; i++)
-    {
-        for (int j = 0; j < b.column; j++)
-        {
-            result.matrix[i][j] = 0;
-            for (int t = 0; t < b.row; t++)
-            {
-                result.matrix[i][j] += a.matrix[i][t] * b.matrix[t][j];
-            }
+    auto rows = lst.begin();
+    for (size_t i = 0; i < M; i++, rows++) {
+        if (rows->size() != N) {
+            delete this;
+            throw(1);
+        }
+
+        auto column = rows->begin();
+        for (size_t j = 0; j < N; j++, column++) {
+            values[i][j] = *column;
         }
     }
-    return result;
 }
 
-Matrix operator+(const Matrix& a, const Matrix& b)
+template <typename T, int M, int N>
+std::ostream& operator<< (std::ostream& out, const Matrix<T, M, N>& m1)
 {
-    cout << "Сумма матриц равна: " << endl;
-    Matrix result(a.row, a.column);
-    for (int i = 0; i < a.row; i++)
-    {
-        for (int j = 0; j < a.column; j++)
-        {
-            result.matrix[i][j] = a.matrix[i][j] + b.matrix[i][j];
+    for (int i = 0; i < m1.rows_n(); i++) {
+        for (int j = 0; j < m1.col_n(); j++) {
+            out << m1[i][j] << " ";
         }
-    }
-    return result;
-}
-
-ostream& operator<<(ostream& out, const Matrix& m) //ф-ция для вывода матрицы
-{
-    for (int i = 0; i < m.row; i++)
-    {
-        for (int j = 0; j < m.column; j++)
-            out << m.matrix[i][j] << " ";
-        out << endl;
     }
     return out;
 }
 
-int main()
-{
-    setlocale(LC_ALL, "rus");
-
-    cout << "Выберите операцию: \n 1) Сложение матриц \n 2) Умножение матриц \n 3) Умножение матрицы на вектор" << endl;
-    int n;
-    cin >> n;
-
-    switch (n)
-    {
-        case 1: // ввод для сложения
-        {
-            int r1, c1, r2, c2;
-            cout << "Введите количество строк матриц: ";
-            cin >> r1;
-            r2 = r1;
-            cout << "Введите количество столбцов матриц: ";
-            cin >> c1;
-            c2 = c1;
-            cout << "Введите элементы 1 матрицы: ";
-
-            Matrix mat(r1, c1);
-            mat.EnterMatrix();
-            cout << mat;
-
-            cout << "Введите элементы 2 матрицы: ";
-
-            Matrix mat2(r2, c2);
-            mat2.EnterMatrix();
-            cout << mat2;
-
-            cout << endl;
-
-            cout << mat + mat2;
-            break;
-        }
-        case 2: // ввод для умножения между матрицами
-        {
-            int r1, c1, r2, c2;
-            cout << "Введите количество строк для 1 матрицы: ";
-            cin >> r1;
-            cout << "Введите количество столбцов для 1 матрицы: ";
-            cin >> c1;
-
-            r2 = c1;
-
-            cout << "Введите элементы 1 матрицы: ";
-
-            Matrix mat(r1, c1);
-            mat.EnterMatrix();
-            cout << mat;
-
-            cout << "Количество строк для 2 матрицы: " << r2 << endl;
-            cout << "Введите количество столбцов для 2 матрицы: ";
-            cin >> c2;
-
-            cout << "Введите элементы 2 матрицы: ";
-
-            Matrix mat2(r2, c2);
-            mat2.EnterMatrix();
-            cout << mat2;
-
-            cout << endl;
-
-            cout << mat * mat2;
-            break;
-        }
-        case 3: // ввод для умножения матрицы на вектор
-        {
-            int r1, c1, r2;
-            cout << "Введите количество строк для 1 матрицы: ";
-            cin >> r1;
-            cout << "Введите количество столбцов для 1 матрицы: ";
-            cin >> c1;
-
-            r2 = c1;
-
-            cout << "Введите элементы 1 матрицы: ";
-
-            Matrix mat(r1, c1);
-            mat.EnterMatrix();
-            cout << mat;
-
-            cout << "Размерность вектора: " << r2 << endl;
-
-            cout << "Введите элементы вектора: ";
-
-            Matrix mat2(r2, 1);
-            mat2.EnterMatrix();
-            cout << mat2;
-
-            cout << endl;
-
-            cout << mat * mat2;
-            break;
-        }
-    }
-    return 0;
+template <typename T, int M, int N>
+Matrix<T, M, N>& Matrix<T, M, N>::operator=(const Matrix<T, M, N>& other) {
+    values = other.values;
+    return *this;
 }
 
+template <typename T, int M, int N>
+array<T, N>& Matrix<T, M, N>::operator[](int col) {
+    return values[col];
+}
+
+template <typename T, int M, int N>
+const array<T, N>& Matrix<T, M, N>::operator[](int col) const {
+    return values[col];
+}
+
+template <typename T, int M, int N>
+Matrix<T, M, N>& operator+(const Matrix<T, M, N>& m1, const Matrix<T, M, N>& m2) {
+    Matrix<T, M, N> result;
+    for (size_t i = 0; i < M; i++) {
+        for (size_t j = 0; j < N; j++) {
+            result[i][j] = m1[i][j] + m2[i][j];
+        }
+    }
+    return result;
+}
+
+template <typename T, int M, int N>
+Matrix<T, M, N>& operator-(const Matrix<T, M, N>& m1, const Matrix<T, M, N>& m2) {
+    Matrix<T, M, N> result;
+    for (size_t i = 0; i < M; i++) {
+        for (size_t j = 0; j < N; j++) {
+            result[i][j] = m1[i][j] - m2[i][j];
+        }
+    }
+    return result;
+}
+
+template <typename T, int M, int N, int K>
+Matrix<T, M, N>& operator*(const Matrix<T, M, K>& m1, const Matrix<T, K, N>& m2) {
+    Matrix<T, M, N> result;
+    for (size_t i = 0; i < M; i++) {
+        for (size_t j = 0; j < N; j++) {
+            for (size_t k = 0; k < K; k++) {
+                result[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    return result;
+}
+
+
+template <typename T, int M, int N>
+Matrix<T, M, N>& Matrix<T, M, N>::operator=(Matrix<T, M, N>&& other) {
+    values = move(other.values);
+    return *this;
+}
+
+int main() {
+    Matrix<int, 2, 2> m;
+    m[0][0] = 1;
+    std::cout << m[0][0] << std::endl;
+    Matrix<int, 2, 2> m1 = {{1, 2},{3, 4}};
+    Matrix<int, 2, 2> m2;
+    m2 = m + m1;
+    m = m1 * m2;
+    std::cout << m[0][0] << std::endl;
+}
